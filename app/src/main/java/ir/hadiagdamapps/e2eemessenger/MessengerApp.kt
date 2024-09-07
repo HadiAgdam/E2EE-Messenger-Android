be4cc73b1de33032f06aad53ea4dafd4f762d677
HadiAgdam <hadiagdamdeveloper@gmail.com>
2024-09-07 16:38:02 +0330

    added volley request init

--- app/src/main/java/ir/hadiagdamapps/e2eemessenger/MessengerApp.kt
+++ app/src/main/java/ir/hadiagdamapps/e2eemessenger/MessengerApp.kt
@@ -1,10 +1,12 @@
 package ir.hadiagdamapps.e2eemessenger
 
 import android.app.Application
+import ir.hadiagdamapps.e2eemessenger.data.network.Network
 
 class MessengerApp: Application() {
 
     override fun onCreate() {
         super.onCreate()
+        Network.initQueue(this)
     }
 }
