bfd41cd49d0e84c4a79db93c7d85784cde9689af
HadiAgdam <hadiagdamdeveloper@gmail.com>
2024-09-08 15:13:40 +0330

    deleted network init

--- app/src/main/java/ir/hadiagdamapps/e2eemessenger/MessengerApp.kt
+++ app/src/main/java/ir/hadiagdamapps/e2eemessenger/MessengerApp.kt
@@ -1,12 +1,10 @@
 package ir.hadiagdamapps.e2eemessenger
 
 import android.app.Application
-import ir.hadiagdamapps.e2eemessenger.data.network.Network
 
 class MessengerApp: Application() {
 
     override fun onCreate() {
         super.onCreate()
-        Network.initQueue(this)
     }
 }
