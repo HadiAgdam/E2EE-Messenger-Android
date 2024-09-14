4edd769a5fc710e17ed0268bbc5aab1e1ca80a36
HadiAgdam <hadiagdamdeveloper@gmail.com>
2024-09-14 21:23:22 +0330

    added pin dialog

--- app/src/main/java/ir/hadiagdamapps/e2eemessenger/ui/screens/ChooseInboxScreen.kt
+++ app/src/main/java/ir/hadiagdamapps/e2eemessenger/ui/screens/ChooseInboxScreen.kt
@@ -1,16 +1,16 @@
 package ir.hadiagdamapps.e2eemessenger.ui.screens
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import ir.hadiagdamapps.e2eemessenger.data.models.InboxModel
-import ir.hadiagdamapps.e2eemessenger.ui.components.InboxDetailsDialog
+import ir.hadiagdamapps.e2eemessenger.ui.components.dialog.InboxDetailsDialog
 import ir.hadiagdamapps.e2eemessenger.ui.components.InboxItem
 import ir.hadiagdamapps.e2eemessenger.ui.components.Screen
+import ir.hadiagdamapps.e2eemessenger.ui.components.dialog.PinDialog
 import ir.hadiagdamapps.e2eemessenger.ui.viewmodels.ChooseInboxViewModel
 
 @Composable
@@ -18,8 +18,7 @@ fun ChooseInboxScreen(viewModel: ChooseInboxViewModel) {
 
 
     Screen(
-        title = "Choose Inbox",
-        fabClick = viewModel::newInbox
+        title = "Choose Inbox", fabClick = viewModel::newInbox
     ) {
 
         LazyColumn(
@@ -29,25 +28,30 @@ fun ChooseInboxScreen(viewModel: ChooseInboxViewModel) {
             items(viewModel.inboxes) { inbox: InboxModel ->
                 InboxItem(text = inbox.label, modifier = Modifier.clickable {
                     viewModel.inboxClick(inbox)
-                },
-                    moreClick = {
-                        viewModel.showDialog(inbox)
-                    }
-                )
+                }, moreClick = {
+                    viewModel.showInboxDialog(inbox)
+                })
             }
 
 
         }
 
 
-        if (viewModel.inboxDialog.value != null)
-            InboxDetailsDialog(
-                viewModel.inboxDialog.value!!,
-                dismiss = viewModel::dialogDismiss,
-                copyPublicKey = viewModel::dialogCopyPublicKey,
-                deleteInbox = viewModel::dialogDeleteInbox,
-                labelValueChange = viewModel::dialogLabelChange
-            )
+        if (viewModel.inboxDialog.value != null) InboxDetailsDialog(
+            viewModel.inboxDialog.value!!,
+            dismiss = viewModel::inboxDialogDismiss,
+            copyPublicKey = viewModel::inboxDialogCopyPublicKey,
+            deleteInbox = viewModel::dialogDeleteInbox,
+            labelValueChange = viewModel::inboxDialogLabelChange
+        )
+
+        if (viewModel.pin.value != null) PinDialog(
+            pin = viewModel.pin.value!!,
+            onPinChanged = viewModel::pinChanged,
+            error = viewModel.pinDialogError.value,
+            dismiss = viewModel::pinDialogDismiss,
+            okClick = viewModel::pinDialogSubmit
+        )
 
     }
 }
