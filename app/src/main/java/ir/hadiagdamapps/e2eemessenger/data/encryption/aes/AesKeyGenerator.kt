6a9a20b811fafc7f98e341efc6c12fa6db20ca5f
HadiAgdam <hadiagdamdeveloper@gmail.com>
2024-09-15 13:57:00 +0330

    added generate key by pin function

--- app/src/main/java/ir/hadiagdamapps/e2eemessenger/data/encryption/aes/AesKeyGenerator.kt
+++ app/src/main/java/ir/hadiagdamapps/e2eemessenger/data/encryption/aes/AesKeyGenerator.kt
@@ -2,10 +2,14 @@ package ir.hadiagdamapps.e2eemessenger.data.encryption.aes
 
 import java.security.SecureRandom
 import java.util.Base64
+import java.util.UUID
 import javax.crypto.Cipher
 import javax.crypto.KeyGenerator
 import javax.crypto.SecretKey
+import javax.crypto.SecretKeyFactory
 import javax.crypto.spec.IvParameterSpec
+import javax.crypto.spec.PBEKeySpec
+import javax.crypto.spec.SecretKeySpec
 
 object AesKeyGenerator {
 
@@ -15,7 +19,17 @@ object AesKeyGenerator {
         return keyGen.generateKey()
     }
 
+    fun generateKey(pin: String, salt: String): SecretKey {
+        val spec = PBEKeySpec(pin.toCharArray(), Base64.getDecoder().decode(salt), 10000, 256)
+        val factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256")
+        val keyBytes = factory.generateSecret(spec).encoded
+        return SecretKeySpec(keyBytes, "AES")
+    }
 
-
+    fun generateSalt(): String {
+        val salt = ByteArray(16)
+        SecureRandom().nextBytes(salt)
+        return Base64.getEncoder().encodeToString(salt)
+    }
 
 }
