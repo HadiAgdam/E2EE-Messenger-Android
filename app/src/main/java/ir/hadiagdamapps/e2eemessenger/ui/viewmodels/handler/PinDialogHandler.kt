3c823a4d35a9b33824747dc2be2ed521faafbd3a
HadiAgdam <hadiagdamdeveloper@gmail.com>
2024-09-15 14:00:47 +0330

    changed pin length from 4 to 6

--- app/src/main/java/ir/hadiagdamapps/e2eemessenger/ui/viewmodels/handler/PinDialogHandler.kt
+++ app/src/main/java/ir/hadiagdamapps/e2eemessenger/ui/viewmodels/handler/PinDialogHandler.kt
@@ -7,19 +7,21 @@ class PinDialogHandler(
     private val submitPin: (pin: String) -> Unit
 ) {
 
+    private val pinLength = 6
     val pin: MutableLiveData<String?> = MutableLiveData(null)
     val pinDialogError: MutableLiveData<String?> = MutableLiveData(null)
 
 
     fun pinChanged(newPin: String) {
-        if (newPin.length < 5)
+        if (newPin.length < pinLength + 1)
             pin.value = newPin
         pinDialogError.value = null
     }
 
 
     fun submit() { // WTF am I doing ?
-        if (pin.value != null && pin.value!!.isDigitsOnly() && pin.value!!.length == 4) submitPin(pin.value!!)
+        if (pin.value != null && pin.value!!.isDigitsOnly() && pin.value!!.length == pinLength)
+            submitPin(pin.value!!)
         else pinDialogError.value = "invalid pin"
 
     }
