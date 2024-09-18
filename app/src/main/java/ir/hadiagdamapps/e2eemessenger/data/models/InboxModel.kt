112c6dafbeca3b11aaf5ca0d5fa1541664d643dd
HadiAgdam <hadiagdamdeveloper@gmail.com>
2024-09-18 19:41:06 +0330

    added serializable and label text replacement for display purpose

--- app/src/main/java/ir/hadiagdamapps/e2eemessenger/data/models/InboxModel.kt
+++ app/src/main/java/ir/hadiagdamapps/e2eemessenger/data/models/InboxModel.kt
@@ -1,7 +1,11 @@
 package ir.hadiagdamapps.e2eemessenger.data.models
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class InboxModel(
     val publicKey: String,
     val encryptedPrivateKey: String, // AES encrypted
     var label: String = publicKey
+    var label: String = publicKey.replace("MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAE", ""),
 )
