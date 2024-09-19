8123b0088f69eecc53fc64cfb88cdad6331626ad
HadiAgdam <hadiagdamdeveloper@gmail.com>
2024-09-19 14:23:15 +0330

    added private key encryption test

--- app/src/test/java/ir/hadiagdamapps/e2eemessenger/EncryptionTest.kt
+++ app/src/test/java/ir/hadiagdamapps/e2eemessenger/EncryptionTest.kt
@@ -6,6 +6,7 @@ import ir.hadiagdamapps.e2eemessenger.data.encryption.e2e.E2EEncryptor
 import ir.hadiagdamapps.e2eemessenger.data.encryption.e2e.E2EKeyGenerator
 import ir.hadiagdamapps.e2eemessenger.data.encryption.e2e.E2EKeyGenerator.toText
 import org.bouncycastle.jce.provider.BouncyCastleProvider
+import org.junit.Assert
 import org.junit.Test
 import java.security.Security
 
@@ -66,5 +67,39 @@ class EncryptionTest {
         println(E2EKeyGenerator.generateKeyPair().public.encoded.toText())
 
     }
+
+
+    @Test
+    fun privateKeyEncryptionTest() {
+        val pair = E2EKeyGenerator.generateKeyPair()
+
+        val pin = "1234"
+
+        val salt = AesKeyGenerator.generateSalt()
+        val aesKey = AesKeyGenerator.generateKey(pin, salt)
+
+
+        println("pv ${pair.private.encoded.toText()}")
+
+        val (encrypted, iv) = AesEncryptor.encryptMessage(pair.private.encoded.toText(), aesKey)
+
+
+        println("encrypted pv : $encrypted")
+
+        val aesKey2 = AesKeyGenerator.generateKey(pin, salt)
+
+        val decryptedPrivateKey = AesEncryptor.decryptMessage(encrypted, aesKey2, iv)
+
+
+        println("decrypted pv : $decryptedPrivateKey")
+
+        Assert.assertNotEquals(encrypted, pair.private.encoded.toText())
+        Assert.assertEquals(pair.private.encoded.toText(), decryptedPrivateKey)
+
+        val wrongPinKey = AesKeyGenerator.generateKey("12342", salt)
+        Assert.assertNotEquals(pair.private.encoded.toText(), AesEncryptor.decryptMessage(encrypted, wrongPinKey, iv))
+
+    }
+
 
 }
