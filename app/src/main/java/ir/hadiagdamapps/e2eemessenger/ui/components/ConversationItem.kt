ae9a55075308f9d2b05b946a583fd2a90df8c57a
HadiAgdam <hadiagdamdeveloper@gmail.com>
2024-09-23 12:46:58 +0330

    created conversation item

---
+++ app/src/main/java/ir/hadiagdamapps/e2eemessenger/ui/components/ConversationItem.kt
@@ -0,0 +1,69 @@
+package ir.hadiagdamapps.e2eemessenger.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import ir.hadiagdamapps.e2eemessenger.R
+import ir.hadiagdamapps.e2eemessenger.ui.theme.ColorPalette
+import ir.hadiagdamapps.e2eemessenger.ui.theme.Typography
+
+@Composable
+fun ConversationItem(
+    modifier: Modifier = Modifier,
+    label: String,
+    detailsClick: () -> Unit,
+    lastMessageText: String,
+    timeText: String
+) {
+
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(ColorPalette.background)
+            .padding(12.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = label,
+                style = Typography.titleMedium,
+                color = Color.White,
+                maxLines = 1,
+                modifier = Modifier.weight(.9f),
+                overflow = TextOverflow.Ellipsis
+            )
+
+            Spacer(modifier = Modifier.weight(.05f))
+
+            IconButton(onClick = detailsClick, modifier = Modifier.fillMaxWidth(.1f)) {
+                Icon(
+                    painter = painterResource(id = R.drawable.more_icon),
+                    contentDescription = "more",
+                    modifier = Modifier.fillMaxSize(),
+                    tint = Color.White
+                )
+            }
+        }
+
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+            Text(text = lastMessageText, maxLines = 1, color = ColorPalette.primary, style = Typography.bodyLarge, overflow = TextOverflow.Ellipsis, modifier = Modifier.weight(1f))
+            Text(text = timeText, color = ColorPalette.primary ,style = Typography.bodyLarge)
+        }
+    }
+}
