fa551a3a9bb0d72fbb2090009f884861aa7402d9
HadiAgdam <hadiagdamdeveloper@gmail.com>
2024-09-23 12:47:15 +0330

    added conversation items

--- app/src/main/java/ir/hadiagdamapps/e2eemessenger/ui/screens/InboxScreen.kt
+++ app/src/main/java/ir/hadiagdamapps/e2eemessenger/ui/screens/InboxScreen.kt
@@ -1,27 +1,33 @@
 package ir.hadiagdamapps.e2eemessenger.ui.screens
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Text
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.navigation.NavHostController
+import ir.hadiagdamapps.e2eemessenger.data.TextFormat
+import ir.hadiagdamapps.e2eemessenger.data.models.ConversationModel
+import ir.hadiagdamapps.e2eemessenger.ui.components.ConversationItem
 import ir.hadiagdamapps.e2eemessenger.ui.components.Screen
 import ir.hadiagdamapps.e2eemessenger.ui.components.dialog.PinDialog
-import ir.hadiagdamapps.e2eemessenger.ui.navigation.routes.InboxScreenRoute
 import ir.hadiagdamapps.e2eemessenger.ui.viewmodels.InboxViewModel
 
 @Composable
 fun InboxScreen(viewModel: InboxViewModel) {
     Screen(
         title = "Inbox",
-        fabClick = viewModel::newChat
+        fabClick = viewModel::newConversation
     ) {
         LazyColumn {
-
+            items(viewModel.conversations) { conversation: ConversationModel ->
+                ConversationItem(
+                    modifier = Modifier.clickable { viewModel.conversationClick(conversation) },
+                    label = conversation.label,
+                    detailsClick = { viewModel.conversationClick(conversation) },
+                    lastMessageText = (if (conversation.lastMessage.sent) "You: " else "") + conversation.lastMessage.text,
+                    timeText = TextFormat.timestampToText(conversation.lastMessage.timestamp)
+                )
+            }
         }
 
 
@@ -30,6 +36,7 @@ fun InboxScreen(viewModel: InboxViewModel) {
             onPinChanged = viewModel::pinChanged,
             error = viewModel.pinDialogError,
             okClick = viewModel::pinSubmitClick,
-            dismiss = {}/* really ? */)
+            dismiss = viewModel::dismiss
+        )
     }
 }
