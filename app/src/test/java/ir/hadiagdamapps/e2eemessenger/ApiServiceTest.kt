b9f7eaf34aa0665bd9590b79d18587f00600bee0
HadiAgdam <hadiagdamdeveloper@gmail.com>
2024-10-03 23:58:00 +0330

    Added getmessage test using MockWebServer

--- app/src/test/java/ir/hadiagdamapps/e2eemessenger/ApiServiceTest.kt
+++ app/src/test/java/ir/hadiagdamapps/e2eemessenger/ApiServiceTest.kt
@@ -1,53 +1,88 @@
 package ir.hadiagdamapps.e2eemessenger
 
-import ir.hadiagdamapps.e2eemessenger.data.models.messages.OutgoingMessage
+import ir.hadiagdamapps.e2eemessenger.data.encryption.aes.AesEncryptor
+import ir.hadiagdamapps.e2eemessenger.data.encryption.aes.AesKeyGenerator
+import ir.hadiagdamapps.e2eemessenger.data.encryption.e2e.E2EEncryptor
+import ir.hadiagdamapps.e2eemessenger.data.encryption.e2e.E2EKeyGenerator
+import ir.hadiagdamapps.e2eemessenger.data.encryption.e2e.E2EKeyGenerator.toText
+import ir.hadiagdamapps.e2eemessenger.data.models.messages.IncomingMessage
 import ir.hadiagdamapps.e2eemessenger.data.network.ApiService
-import ir.hadiagdamapps.e2eemessenger.data.network.RetrofitInstance
 import kotlinx.coroutines.runBlocking
-import org.junit.Assert
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.bouncycastle.jce.provider.BouncyCastleProvider
+import org.junit.After
 import org.junit.Before
 import org.junit.Test
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.security.Security
+import kotlin.math.log
 
 class ApiServiceTest {
 
+    private lateinit var mockWebServer: MockWebServer
     private lateinit var apiService: ApiService
 
+
     @Before
-    fun setUp() {
+    fun setup() {
+        mockWebServer = MockWebServer()
+        mockWebServer.start()
 
-        apiService = RetrofitInstance.api
+        apiService = Retrofit.Builder()
+            .baseUrl(mockWebServer.url("/"))
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiService::class.java)
     }
 
-
-
-    @Test
-    fun testNewMessage() = runBlocking {
-        val message = OutgoingMessage(
-            "public key",
-            "encryption key",
-            "message",
-            ""
-        )
-
-
-        val response = apiService.newMessage(message)
-
-
-        Assert.assertTrue(response.isSuccessful)
+    @After
+    fun teardown() {
+        mockWebServer.shutdown()
     }
 
     @Test
-    fun testGetMessage() = runBlocking {
-
-        val response = apiService.getMessage(0, "")
-
-        println()
-        println(response.body())
-        println()
+    fun testGetMessage(): Unit = runBlocking {
+        Security.addProvider(BouncyCastleProvider())
 
 
+        val keyPair = E2EKeyGenerator.generateKeyPair()
+        val aesKey = AesKeyGenerator.generateKey()
 
+        val encryptedAesKey = E2EEncryptor.encryptAESKeyWithPublicKey(aesKey, keyPair.public.encoded.toText())
 
+        val text = "Hello, World !"
+        val encryptedMessage = AesEncryptor.encryptMessage(text, aesKey)
+
+//        val mockResponse = Response.success(IncomingMessage(
+//            messageId = 0,
+//            encryptionKey =  encryptedAesKey,
+//            encryptedMessage = encryptedMessage.first,
+//            time = .0,
+//            iv = encryptedMessage.second
+//        ))
+        val mockResponse = MockResponse()
+            .setResponseCode(200)
+            .setBody("""
+            [
+                {
+                 "messageId": 0,
+                 "encryptionKey": "$encryptedAesKey",
+                 "encryptedMessage": "${encryptedMessage.first}",
+                 "time": 0.0,
+                 "iv": "${encryptedMessage.second}"
+                }
+            ]
+            """.trimIndent())
+
+        mockWebServer.enqueue(mockResponse)
+
+        val response = apiService.getMessage(0, keyPair.public.encoded.toText())
+
+        println(response.body()!!.size.toString())
     }
+
 
 }
