e342129d8de54f71e15d5d40bb9c396dc8c9d8ff
HadiAgdam <hadiagdamdeveloper@gmail.com>
2024-10-05 19:35:03 +0330

    added setUpBouncyCastle function

--- app/src/main/java/ir/hadiagdamapps/e2eemessenger/MessengerApp.kt
+++ app/src/main/java/ir/hadiagdamapps/e2eemessenger/MessengerApp.kt
@@ -4,11 +4,19 @@ import android.app.Application
 import org.bouncycastle.jce.provider.BouncyCastleProvider
 import java.security.Security
 
-class MessengerApp: Application() {
+class MessengerApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        Security.addProvider(BouncyCastleProvider())
+        setupBouncyCastle()
+    }
 
+    private fun setupBouncyCastle() {
+        val provider = Security.getProvider(BouncyCastleProvider.PROVIDER_NAME) ?: return
+        if (provider::class.java == BouncyCastleProvider::class.java) {
+            return
+        }
+        Security.removeProvider(BouncyCastleProvider.PROVIDER_NAME)
+        Security.insertProviderAt(BouncyCastleProvider(), 1)
     }
 }
