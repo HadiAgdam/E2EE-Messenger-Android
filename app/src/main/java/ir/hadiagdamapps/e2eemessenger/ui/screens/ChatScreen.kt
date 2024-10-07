8cfac64c27370a5f46c6fa7310dd35fb842d2adf
HadiAgdam <hadiagdamdeveloper@gmail.com>
2024-10-07 11:01:32 +0330

    added split by day to display messages

--- app/src/main/java/ir/hadiagdamapps/e2eemessenger/ui/screens/ChatScreen.kt
+++ app/src/main/java/ir/hadiagdamapps/e2eemessenger/ui/screens/ChatScreen.kt
@@ -10,9 +10,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import ir.hadiagdamapps.e2eemessenger.ui.components.ChatBox
+import ir.hadiagdamapps.e2eemessenger.ui.components.ChatDateText
 import ir.hadiagdamapps.e2eemessenger.ui.components.ChatItem
 import ir.hadiagdamapps.e2eemessenger.ui.components.Screen
 import ir.hadiagdamapps.e2eemessenger.ui.viewmodels.ChatScreenViewModel
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -23,20 +27,35 @@ fun ChatScreen(viewModel: ChatScreenViewModel) {
     ) {
 
         Column(modifier = Modifier.fillMaxSize()) {
-
+            var t = 0L
             LazyColumn(modifier = Modifier.fillMaxWidth()) {
-                items(viewModel.chats) {
+                items(viewModel.chats) { // items should be sorted
+
+                    if (!isSameDay(t, it.time)) {
+                        ChatDateText(timestamp = it.time)
+                        t = it.time
+                    }
                     ChatItem(text = it.text, timeStamp = it.time, sent = it.sent)
                 }
             }
-            ChatBox(text = viewModel.chatBoxContent, onValueChange = viewModel::chatBoxTextChange) {
-
-            }
+            ChatBox(
+                text = viewModel.chatBoxContent,
+                onValueChange = viewModel::chatBoxTextChange,
+                sendClick = viewModel::chatBoxSubmit
+            )
 
         }
 
     }
 
+}
+
+
+private fun isSameDay(t1: Long, t2: Long): Boolean {
+    val dt1 = LocalDateTime.ofInstant(Instant.ofEpochMilli(t1), ZoneId.systemDefault())
+    val dt2 = LocalDateTime.ofInstant(Instant.ofEpochMilli(t2), ZoneId.systemDefault())
+
+    return dt1.year == dt2.year && dt1.month == dt2.month && dt1.dayOfMonth == dt2.dayOfMonth
 }
 
 
