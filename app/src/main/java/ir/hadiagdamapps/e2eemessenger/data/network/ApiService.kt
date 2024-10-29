83abb688efcdcf2eeab61453c6c47501ec301ceb
HadiAgdam <hadiagdamdeveloper@gmail.com>
2024-10-29 20:55:45 +0330

    added json type header to content type

--- app/src/main/java/ir/hadiagdamapps/e2eemessenger/data/network/ApiService.kt
+++ app/src/main/java/ir/hadiagdamapps/e2eemessenger/data/network/ApiService.kt
@@ -5,12 +5,14 @@ import ir.hadiagdamapps.e2eemessenger.data.models.messages.OutgoingMessage
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface ApiService {
 
     @POST("new_message")
+    @Headers("Content-Type: application/json")
     suspend fun newMessage(@Body message: OutgoingMessage): Response<Unit>
 
     @GET("get_message")
